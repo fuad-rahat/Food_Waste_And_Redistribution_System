2d6f40c71df140f9cf620c2d908e4ee35ac53456
fuad-rahat <fuadrahat01@gmail.com>
2026-03-03 16:32:46 +0600

    added authentication middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'No token' });
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(403).json({ message: 'Invalid token' });
+  }
+};
